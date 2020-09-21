9c416ce641ad4c6bee6a4de747dab9049ffb948e
yyhk123 <minoook@hotmail.com>
2020-09-21 00:54:30 -0500

    add_message

--- index.js
+++ index.js
@@ -13,22 +13,19 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("The sever has started on port: ${port}"));
 
 // set up mongoose
-//const uri = process.env.MONGODB_CONNECTION_STRING
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/heroku_xbwvxbp9",
-  {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-  }
-);
+//const uri = process.env.MONGODB_CONNECTION_STRING;
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("mern-auth-front/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(
-      path.resolve(__dirname, "mern-auth-front", "build", "index.html")
+      path.join(__dirname, "mern-auth-front", "build", "index.html")
     );
   });
 }
